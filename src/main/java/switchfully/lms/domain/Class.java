394b059cb51eb5e65585faf252c18e831b0d422d
nicolas bluj <nicolas_bluj@acslworld.com>
2025-06-03 13:42:38 +0200

    created Class.java domain entity, mapped many to many with users

--- src/main/java/switchfully/lms/domain/Class.java
+++ src/main/java/switchfully/lms/domain/Class.java
@@ -1,5 +1,60 @@
 package switchfully.lms.domain;
 
-public class Class {
+import jakarta.persistence.*;
 
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "classes")
+public class Class {
+    @Id
+    @SequenceGenerator(sequenceName = "classes_seq", name = "classes_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "classes_seq")
+    private Long id;
+
+    @Column(name = "title", nullable = false, unique = true)
+    private String title;
+
+    @ManyToMany(mappedBy = "classes")
+    private List<User> users;
+
+    public Class() {}
+    public Class(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Class that = (Class) o;
+        return this.title.equals(that.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, users);
+    }
+
+    @Override
+    public String toString() {
+        return "Class{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", users=" + users +
+                '}';
+    }
 }
