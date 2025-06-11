96b802c0ecf4831639ec4f9075ef35f5a5b532ea
nicolas bluj <nicolas_bluj@acslworld.com>
2025-06-11 14:38:04 +0200

    😭 trying to have the security working realmreource.client returning empty list

--- src/main/java/switchfully/lms/utility/security/KeycloakService.java
+++ src/main/java/switchfully/lms/utility/security/KeycloakService.java
@@ -3,8 +3,10 @@ package switchfully.lms.utility.security;
 import org.keycloak.admin.client.CreatedResponseUtil;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.resource.ClientResource;
+import org.keycloak.admin.client.resource.ClientsResource;
 import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.admin.client.resource.UserResource;
+import org.keycloak.representations.idm.ClientRepresentation;
 import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
@@ -20,17 +22,25 @@ import java.util.List;
 @Service
 public class KeycloakService {
 
-    private final RealmResource realmRessource;
+    private final RealmResource realmResource;
     private final String clientID;
 
-    public KeycloakService(Keycloak keycloak, @Value("${keycloak.realm}") String realmName, @Value("${keycloak.resource}") String clientId) {
+    public KeycloakService(Keycloak keycloak, @Value("java-2025-03") String realmName, @Value("admin-cli") String clientId) {
         this.clientID = clientId;
-        this.realmRessource = keycloak.realm(realmName);
+        this.realmResource = keycloak.realm(realmName);
     }
 
     // create user in the realm
+    //401 not authorized by keycloak
     private Response createUser(String username) {
-        return realmRessource.users().create(createUserRepresentation(username));
+        return realmResource.users().create(createUserRepresentation(username));
+    }
+
+    private UserRepresentation createUserRepresentation(String username) {
+        UserRepresentation user = new UserRepresentation();
+        user.setUsername(username);
+        user.setEnabled(true);
+        return user;
     }
 
     // create a representation ?
@@ -49,33 +59,42 @@ public class KeycloakService {
         addRole(getUser(createdUserId), keycloakUserDTO.role().name());
     }
     private UserResource getUser(String userId) {
-        return realmRessource.users().get(userId);
+        return realmResource.users().get(userId);
     }
 
 
     private ClientResource getClientResource() {
-        return realmRessource.clients().get(getClientUUID());
+        return realmResource.clients().get(getClientUUID().getId());
     }
 
-    private String getClientUUID() {
-        return realmRessource.clients().findByClientId(clientID).get(0).getId();
+    private ClientRepresentation getClientUUID() {
+        ClientsResource clientResource = realmResource.clients();
+        //NEXT LINE RETURNS AN EMPTY LIST
+        List<ClientRepresentation> clientResources = clientResource.findByClientId(this.clientID);
+        List<ClientRepresentation> client = realmResource.clients().findAll();
+        return realmResource.clients()
+                .findByClientId(clientID)
+                .get(0);
     }
 
-    private UserRepresentation createUserRepresentation(String username) {
-        UserRepresentation user = new UserRepresentation();
-        user.setUsername(username);
-        user.setEnabled(true);
-        return user;
-    }
+
 
 
 
     private void addRole(UserResource user, String roleName) {
-        user.roles().clientLevel(getClientUUID()).add(List.of(getRole(roleName)));
+        RoleRepresentation role = getRole(roleName);
+        List<RoleRepresentation> roles = List.of(role);
+        user.roles()
+                .clientLevel(getClientUUID().getId())
+                .add(roles);
+        //user.roles().clientLevel(getClientUUID()).add(List.of(getRole(roleName)));
     }
 
     private RoleRepresentation getRole(String roleToAdd) {
-        return getClientResource().roles().get(roleToAdd).toRepresentation();
+        return getClientResource()
+                .roles()
+                .get(roleToAdd)
+                .toRepresentation();
     }
 
 
