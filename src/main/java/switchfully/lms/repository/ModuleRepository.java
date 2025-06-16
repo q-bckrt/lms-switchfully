4dc2628a0446e55a02c43291fefe1a3c52f583bf
q-bckrt <pom.schc@egmail.com>
2025-06-16 11:10:11 +0200

    Added javadoc for the repositories of Course, Module, Submodule and Codelab

--- src/main/java/switchfully/lms/repository/ModuleRepository.java
+++ src/main/java/switchfully/lms/repository/ModuleRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import switchfully.lms.domain.Module;
 
+/**
+ * Repository interface for managing Module entities.
+ * It extends JpaRepository to provide CRUD operations.
+ * @see Module
+ * @see JpaRepository
+ */
 @Repository
 public interface ModuleRepository extends JpaRepository<Module, Long> {
 }
--- src/main/java/switchfully/lms/repository/SubmoduleRepository.java
+++ src/main/java/switchfully/lms/repository/SubmoduleRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import switchfully.lms.domain.Submodule;
 
+/**
+ * Repository interface for managing Submodule entities.
+ * It extends JpaRepository to provide CRUD operations.
+ * @see Submodule
+ * @see JpaRepository
+ */
 @Repository
 public interface SubmoduleRepository extends JpaRepository<Submodule, Long> {
 }
