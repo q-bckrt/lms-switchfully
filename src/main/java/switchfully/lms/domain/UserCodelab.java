3c4888cd5f17a41cc484ec542f7aaf5729c94f6b
Marie-Catherine Bouquieaux <mcbouquieaux@gmail.com>
2025-06-16 14:29:19 +0200

    add userCodelabd and embedded ID

--- src/main/java/switchfully/lms/domain/UserCodelab.java
+++ src/main/java/switchfully/lms/domain/UserCodelab.java
@@ -1,4 +1,23 @@
 package switchfully.lms.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users_codelabs")
 public class UserCodelab {
+
+    @EmbeddedId
+    private UserCodelabId id;
+
+    @Column(name="progress", nullable=false)
+    @Enumerated(EnumType.STRING)
+    private ProgressLevel progressLevel;
+
+    public UserCodelab() {
+    }
+
+    public UserCodelab(UserCodelabId id, ProgressLevel progressLevel) {
+        this.id = id;
+        this.progressLevel = progressLevel;
+    }
 }
--- src/main/java/switchfully/lms/repository/UserCodelabRepository.java
+++ src/main/java/switchfully/lms/repository/UserCodelabRepository.java
@@ -1,4 +1,13 @@
 package switchfully.lms.repository;
 
-public interface UserCodelabRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import switchfully.lms.domain.UserCodelab;
+import switchfully.lms.domain.UserCodelabId;
+
+import java.util.List;
+
+public interface UserCodelabRepository extends JpaRepository<UserCodelab, UserCodelabId> {
+
+    List<UserCodelab> findByIdUserId(Long userId);
+    List<UserCodelab> findByIdCodelabId(Long codelabId);
 }
